cb73900b2fb3aa173bc7971ab9a99f88a262a1ed
goraasep <gora.asep@gmail.com>
2024-09-12 12:29:08 +0700

    feat: first init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.TodoApp.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("======= \t EXERCISE DAY 9 \t=======");
+        MainMenu menu = new MainMenu();
+        menu.run();
+    }
+}
---
+++ src/main/java/org/example/TodoApp/Todo.java
@@ -0,0 +1,21 @@
+package org.example.TodoApp;
+
+import java.util.UUID;
+
+public class Todo {
+    private UUID id;
+    private String text;
+
+    Todo(String text) {
+        this.id = UUID.randomUUID();
+        this.text = text;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
